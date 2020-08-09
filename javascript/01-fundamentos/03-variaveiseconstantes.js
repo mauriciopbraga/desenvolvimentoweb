57b041660602b163d028380e7f51a081780aacaa
Mauricio P. Braga <mauriciopereirabraga@gmail.com>
2020-08-09 02:04:10 -0300

    js-fundamentos

---
+++ javascript/01-fundamentos/03-variaveiseconstantes.js
@@ -0,0 +1,22 @@
+var numero1 = 5;
+let numero2 = 2; // Usar esse
+
+console.log(numero1, numero2);
+
+var numero1 = 30; // JS é estranho. Assim funciona!
+// let numero2 = 48; - Gera erro, pois já foi declarado como "let" lá em cima
+numero2 = 48; // Assim funciona normal
+
+console.log(numero1, numero2);
+
+// Os dois funcionam normalmente
+numero1 = 97;
+numero2 = 65;
+
+console.log(numero1, numero2);
+
+const numero3 = 7;
+
+// numero3 = 54; - Dá erro, pois uma constante não pode ser alterada
+
+console.log(numero3);
